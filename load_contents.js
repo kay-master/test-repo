52b5d3c38a130b20b54501c9b58e4595fe8efd4f
Kuhle Hanisi <hanisikuhle@gmail.com>
2022-10-30 15:30:00 +0200

    Update load_contents.js

--- load_contents.js
+++ load_contents.js
@@ -1,37 +1,87 @@
 const fs = require('fs');
 const path = process.argv[2];
 
-function getFileContents(path, callback) {
-  fs.exists(path, (exists) => {
-    if (exists) {
-      console.log('exists');
-      fs.stat(path, (err, stats) => {
-        if (err) {
-          return callback(new Error('Error trying to get stats'));
+async function openFile(path) {
+  return new Promise((resolve, reject) => {
+    fs.open(path, 'r', (err, fd) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          reject(new Error('File does not exist'))
+          return;
         }
-        console.log(stats);
-        if (stats.size > 0) {
-          fs.readFile(path, (err, buffer) => {
-            if (err) {
-              return callback(new Error('Error trying to get stats'));
-            }
-            return callback(null, buffer);
-          })
-        } else {
-          return callback(new Error('File exists but there is no content'));
-        }
-      })
-    } else {
-      return callback(new Error('File does not exist'));
-    }
+
+        reject(err)
+      }
+
+      resolve(fd)
+    });
   })
 }
 
+async function getStats(path) {
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        reject(new Error('Error trying to get stats'));
+      }
+
+      if (stats.size > 0) {
+        resolve(stats)
+      }
+
+      reject(new Error('File exists but there is no content'));
+    })
+  })
+}
+
+async function getFileContents(path, callback) {
+
+  let fileDescriptor = 0;
+
+  try {
+    if (!path) {
+      console.error(`Path is required`);
+      return
+    }
+
+    fileDescriptor = await openFile(path);
+
+    await getStats(path);
+
+    fs.read(fileDescriptor, (err, bytes, buffer) => {
+
+      if (err) {
+        return callback(err);
+      }
+
+      return callback(null, { buffer, bytes });
+    })
+  } catch (error) {
+    // Close the opened file.
+    fs.close(fileDescriptor, (err) => {
+      if (err) {
+        console.log(err);
+      }
+
+      console.log("\nFile closed successfully\n");
+    });
+
+    callback(error);
+  }
+}
 
 getFileContents(path, (err, contents) => {
   if (err) {
-    console.error(`There was an error getting contents for ${path}`, err);
+    console.error(`There was an error getting contents for ${path}\n`, err);
     return;
   }
+
   console.info('File was found and the contents were loaded');
+
+  if (contents.bytes > 0) {
+    console.log(contents.buffer.
+      slice(0, contents.bytes).toString());
+  }
+
+  console.log(contents.bytes + " bytes read");
 });
